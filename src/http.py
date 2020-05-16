73e21ec086bdb5d00b1f7eeb7f12f16287291f71
burnhamup <chris@burnhamup.com>
2020-05-15 22:40:56 -0700

    Rewrite the HTTP client to use a CookieJar
    The AUTH cookie and incapsula cookies change with subsequent requests to the IndieGala server. By tracking those cookies over time I hope to behave nicer and extend the length of my session.

---
+++ src/http.py
@@ -0,0 +1,52 @@
+import logging
+
+import aiohttp
+from galaxy.api.errors import AuthenticationRequired
+from galaxy.http import create_client_session
+from yarl import URL
+
+
+class CookieJar(aiohttp.CookieJar):
+    # Inspired by from https://github.com/TouwaStar/Galaxy_Plugin_Bethesda/blob/master/betty/http_client.py
+    def __init__(self):
+        super().__init__()
+        self._cookies_updated_callback = None
+
+    def set_cookies_updated_callback(self, callback):
+        self._cookies_updated_callback = callback
+
+    def update_cookies(self, cookies, url=URL()):
+        super().update_cookies(cookies, url)
+        if cookies and self._cookies_updated_callback:
+            all_cookies = {cookie.key: cookie.value for cookie in self}
+            self._cookies_updated_callback(all_cookies)
+
+
+class HTTPClient(object):
+    """
+    Intended to store and track cookies and update them on each request
+    """
+    def __init__(self, store_credentials):
+        self.cookieJar = CookieJar()
+        self.cookieJar.set_cookies_updated_callback(store_credentials)
+        self.session = create_client_session(cookie_jar=self.cookieJar)
+
+    async def get(self, url):
+        """
+        returns the url and updates the cookies
+        :param url:
+        :return:
+        """
+        logging.debug('Calling HTTPClient.get with %s', url)
+        response = await self.session.get(url)
+        text = await response.text()
+        if '_Incapsula_Resource' in text:
+            logging.debug('Incapsula challenge on request for %s', url)
+            raise AuthenticationRequired()
+        if 'Profile locked' in text:
+            logging.debug('IP check required on request for %s', url)
+            raise AuthenticationRequired()
+        return text
+
+    def update_cookies(self, cookies):
+        self.cookieJar.update_cookies(cookies)
--- src/plugin.py
+++ src/plugin.py
@@ -8,7 +8,8 @@ from galaxy.api.plugin import Plugin, create_and_run_plugin
 from galaxy.api.consts import Platform, LicenseType
 from galaxy.api.types import NextStep, Authentication, Game, LicenseInfo
 from galaxy.api.errors import AuthenticationRequired
-from galaxy.http import HttpClient
+
+from .http import HTTPClient
 
 with open(Path(__file__).parent / 'manifest.json', 'r') as f:
     __version__ = json.load(f)['version']
@@ -46,14 +47,14 @@ class IndieGalaPlugin(Plugin):
             writer,
             token
         )
-        self.http_client = HttpClient()
+        self.http_client = HTTPClient(self.store_credentials)
         self.session_cookie = None
 
     # implement methods
     async def authenticate(self, stored_credentials=None):
         if not stored_credentials:
             return NextStep("web_session", AUTH_PARAMS)
-        self.session_cookies = stored_credentials
+        self.http_client.update_cookies(stored_credentials)
         try:
             return await self.get_user_info()
         except AuthenticationRequired:
@@ -62,52 +63,33 @@ class IndieGalaPlugin(Plugin):
     async def pass_login_credentials(self, step, credentials, cookies):
         """Called just after CEF authentication (called as NextStep by authenticate)"""
         session_cookies = {cookie['name']: cookie['value'] for cookie in cookies if cookie['name']}
-        self.store_credentials(session_cookies)
-        self.session_cookies = session_cookies
+        self.http_client.update_cookies(session_cookies)
         return await self.get_user_info()
 
     async def get_owned_games(self):
         page = 1
         games = []
         while True:
-            raw_html = await self.retrieve_showcase_html(page)
+            try:
+                raw_html = await self.retrieve_showcase_html(page)
+            except AuthenticationRequired:
+                self.lost_authentication()
+                raise
             if 'Your showcase list is actually empty.' in raw_html:
                 return games
-            if 'Profile locked' in raw_html:
-                logging.debug('IP check required')
-                self.lost_authentication()
-                raise AuthenticationRequired()
-            if '_Incapsula_Resource' in raw_html:
-                logging.debug('Incapsula challenge on showcase page %s', page)
-                self.lost_authentication()
-                raise AuthenticationRequired()
             soup = BeautifulSoup(raw_html)
             games.extend(self.parse_html_into_games(soup))
             page += 1
 
     async def get_user_info(self):
-        if not self.session_cookies:
-            raise AuthenticationRequired()
-        response = await self.http_client.request('get', HOMEPAGE, cookies=self.session_cookies, allow_redirects=False)
-        text = await response.text()
-        if '_Incapsula_Resource' in text:
-            logging.debug('Incapsula challenge on get_user_info')
-            # TODO try returning a NextStep() to open a browser. Can I open a next step to /library?
-            raise AuthenticationRequired()
-        if 'Profile locked' in text:
-            logging.debug('IP check required')
-            raise AuthenticationRequired()
+        text = await self.http_client.get(HOMEPAGE)
         soup = BeautifulSoup(text)
         username_div = soup.select('div.username-text')[0]
         username = str(username_div.string)
         return Authentication(username, username)
 
     async def retrieve_showcase_html(self, n=1):
-        if not self.session_cookies:
-            raise AuthenticationRequired()
-        response = await self.http_client.request('get', SHOWCASE_URL % n, cookies=self.session_cookies, allow_redirects=False)
-        text = await response.text()
-        return text
+        return await self.http_client.get(SHOWCASE_URL % n)
 
     @staticmethod
     def parse_html_into_games(soup):
